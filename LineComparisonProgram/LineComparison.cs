e88c6cd5710b60ecf5d8105c5caec688c6770d90
abh224272 <abh224272@gmail.com>
2020-12-16 19:33:05 +0530

    [Abhishek Mishra] Add: Calculation of Length of A Line

---
+++ LineComparisonProgram/LineComparison.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonProgram
+{
+    class LineComparison
+    {
+        public double xAxisStart, yAxisStart, xAxisEnd, yAxisEnd;
+        public void AcceptPoints()
+        {
+            Console.WriteLine("Enter X Start");
+            xAxisStart = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y Start");
+            yAxisStart = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X End");
+            xAxisEnd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y End");
+            yAxisEnd = Convert.ToDouble(Console.ReadLine());
+        }
+       
+        public double GetLength()
+        {
+            double length = Math.Sqrt(Math.Pow( (xAxisEnd - xAxisStart),2 ) + Math.Pow( (yAxisEnd-yAxisStart),2 ) );
+            return length;
+        }
+    }
+}
--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -5,8 +5,13 @@ namespace LineComparisonProgram
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.WriteLine("Welcome To Line Comparison Program");
+            LineComparison lineComparison = new LineComparison();
+
+            lineComparison.AcceptPoints();
+            double lineLength = lineComparison.GetLength();
+            Console.WriteLine("Length of first Line according to points is= " + lineLength);
         }
     }
 }
