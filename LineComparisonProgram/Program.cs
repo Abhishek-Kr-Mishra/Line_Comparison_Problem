39421e408b22e3d92f757b6b8a1befe8850d3fa5
abh224272 <abh224272@gmail.com>
2020-12-16 20:50:23 +0530

    [Abhishek Mishra] Add: Compared both the Lines length using Equals method

--- LineComparisonProgram/Program.cs
+++ LineComparisonProgram/Program.cs
@@ -7,11 +7,31 @@ namespace LineComparisonProgram
         static void Main(string[] args)
         { 
             Console.WriteLine("Welcome To Line Comparison Program");
-            LineComparison lineComparison = new LineComparison();
+            LineComparison firstLine = new LineComparison();
+            LineComparison secondLine = new LineComparison();
 
-            lineComparison.AcceptPoints();
-            double lineLength = lineComparison.GetLength();
-            Console.WriteLine("Length of first Line according to points is= " + lineLength);
+            firstLine.AcceptPoints();
+            double firstLineLength = firstLine.GetLength();
+            Console.WriteLine("Length of first Line according to points is= " + firstLineLength);
+
+            secondLine.AcceptPoints();
+            double secondLineLength = secondLine.GetLength();
+            Console.WriteLine("Length of Second Line according to points is= " + secondLineLength);
+
+            bool status = LengthComparison(firstLineLength, secondLineLength);
+            if (status)
+            {
+                Console.WriteLine("Both the lengths are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Lengths are not Equal");
+            }
         }
+
+        public static Boolean LengthComparison(double firstLength, double secondLength)
+        {
+            return firstLength.Equals(secondLength);
+        } 
     }
 }
